122c865ac5e97258087fb774e6b0fc9dd87c019a
Jorge Lopez Bauer <jorge.lopezbauer@ing.austral.edu.ar>
2022-10-21 14:48:42 -0300

    Division

---
+++ src/main/scala/composite/composite/implementation/Division.scala
@@ -0,0 +1,9 @@
+package composite.composite.implementation
+
+import composite.FunctionComponent
+import composite.composite.TwoComponentsFunction
+
+case class Division(leftFunctionComponent: FunctionComponent, rightFunctionComponent: FunctionComponent)
+  extends TwoComponentsFunction(leftFunctionComponent: FunctionComponent, rightFunctionComponent: FunctionComponent):
+  def calculate(): Double =
+    leftFunctionComponent.calculate() / rightFunctionComponent.calculate()
---
+++ src/test/scala/function_solve/DivisionTest.scala
@@ -0,0 +1,39 @@
+package function_solve
+
+import composite.component.*
+import composite.composite.implementation.Division
+import org.scalatest.funsuite.AnyFunSuite
+import util.variable_input_method.DefinedVariableInputMethod
+
+class DivisionTest extends AnyFunSuite {
+
+  test("3 / 2 = 1.5") {
+    val division = Division(NumberComponent(3), NumberComponent(2))
+    val result = division.calculate()
+    assert(result == 1.5)
+  }
+
+  test("9 / 3 / 2 = 1.5") {
+    val division = Division(Division(NumberComponent(9), NumberComponent(3)) ,NumberComponent(2))
+    val result = division.calculate()
+    assert(result == 1.5)
+  }
+
+  test("a=1 / 2 = 0.5") {
+    val division = Division(VariableComponent("a", DefinedVariableInputMethod(1)), NumberComponent(2))
+    val result = division.calculate()
+    assert(result == 0.5)
+  }
+
+  test("3 / -2 = -1.5") {
+    val division = Division(NumberComponent(3), NumberComponent(-2))
+    val result = division.calculate()
+    assert(result == -1.5)
+  }
+
+  test("-3 / -2 = 1.5") {
+    val division = Division(NumberComponent(-3), NumberComponent(-2))
+    val result = division.calculate()
+    assert(result == 1.5)
+  }
+}
