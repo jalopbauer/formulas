f0675ea45a3a1c55c35ebc7c1db2b12ffa89c71c
Jorge Lopez Bauer <jorge.lopezbauer@ing.austral.edu.ar>
2022-10-21 17:12:35 -0300

    Abs and pow

---
+++ src/main/scala/composite/composite/implementation/Abs.scala
@@ -0,0 +1,9 @@
+package composite.composite.implementation
+
+import composite.FunctionComponent
+import composite.composite.SingleComponentFunction
+
+case class Abs(functionComponent: FunctionComponent)
+  extends SingleComponentFunction(functionComponent: FunctionComponent):
+  def calculate(): Double =
+    functionComponent.calculate().abs
---
+++ src/main/scala/composite/composite/implementation/Power.scala
@@ -0,0 +1,11 @@
+package composite.composite.implementation
+
+import composite.FunctionComponent
+import composite.composite.TwoComponentsFunction
+
+import scala.math.pow
+
+case class Power(leftFunctionComponent: FunctionComponent, rightFunctionComponent: FunctionComponent)
+  extends TwoComponentsFunction(leftFunctionComponent: FunctionComponent, rightFunctionComponent: FunctionComponent):
+  def calculate(): Double =
+    pow(leftFunctionComponent.calculate(),rightFunctionComponent.calculate())
