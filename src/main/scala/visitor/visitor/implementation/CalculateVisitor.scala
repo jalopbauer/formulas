066934ad5f7e9bbe4b70cd4ca185032c3d74f4d9
Jorge Lopez Bauer <jorge.lopezbauer@ing.austral.edu.ar>
2022-10-22 19:36:44 -0300

    CalculateVisitor

---
+++ src/main/scala/visitor/visitor/implementation/CalculateVisitor.scala
@@ -0,0 +1,35 @@
+package visitor.visitor.implementation
+
+import visitor.component.VariableComponent
+import visitor.visitor.FunctionVisitor
+import visitor.component.*
+
+case class CalculateVisitor(totalValue:Double) extends FunctionVisitor():
+  def visit(functionComponent:FunctionComponent): CalculateVisitor = functionComponent match
+    case variableComponent: VariableComponent => CalculateVisitor(variableComponent.variableInputMethod.receiveVariableValue(variableComponent.name))
+    case numberComponent: NumberComponent => CalculateVisitor(numberComponent.number)
+    case sum: Sum =>
+      CalculateVisitor(sum.functionComponents.map(functionComponent => CalculateVisitor(0d).visit(functionComponent).totalValue).sum)
+    case multiplication: Multiplication =>
+      CalculateVisitor(multiplication.functionComponents.map(functionComponent => CalculateVisitor(1d).visit(functionComponent).totalValue).foldLeft(1d)((acc, total) => acc * total))
+    case division: Division =>
+      CalculateVisitor(
+        CalculateVisitor(0d).visit(division.leftFunctionComponent).totalValue
+          /
+        CalculateVisitor(0d).visit(division.rightFunctionComponent).totalValue)
+    case power: Power =>
+      CalculateVisitor(
+        CalculateVisitor(0d).visit(power.leftFunctionComponent).totalValue
+          /
+        CalculateVisitor(0d).visit(power.rightFunctionComponent).totalValue)
+    case subtraction: Subtraction =>
+      CalculateVisitor(
+        CalculateVisitor(0d).visit(subtraction.leftFunctionComponent).totalValue
+          /
+        CalculateVisitor(0d).visit(subtraction.rightFunctionComponent).totalValue)
+    case abs: Abs =>
+      CalculateVisitor(CalculateVisitor(0d).visit(abs.functionComponent).totalValue.abs)
+//
+//  def helperFunction(variableComponentList: List[FunctionComponent]):Double =
+//    sum.functionComponents.foldLeft(0d)((acc, functionComponent) => acc + functionComponent.calculate)
+//    variableComponentList.flatMap(component => GetVariablesVisitor(0d).visit(component).variableComponentList)
