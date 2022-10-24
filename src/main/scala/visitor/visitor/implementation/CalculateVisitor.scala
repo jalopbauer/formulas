02e8dd91146e8e6073b04041faa13a309eaf0ee9
Jorge Lopez Bauer <jorge.lopezbauer@ing.austral.edu.ar>
2022-10-24 11:08:11 -0300

    CalculateVisitor

--- src/main/scala/visitor/visitor/implementation/CalculateVisitor.scala
+++ src/main/scala/visitor/visitor/implementation/CalculateVisitor.scala
@@ -4,6 +4,8 @@ import visitor.component.VariableComponent
 import visitor.visitor.FunctionVisitor
 import visitor.component.*
 
+import scala.math.pow
+
 case class CalculateVisitor(totalValue:Double) extends FunctionVisitor():
   def visit(functionComponent:FunctionComponent): CalculateVisitor = functionComponent match
     case variableComponent: VariableComponent => CalculateVisitor(variableComponent.variableInputMethod.receiveVariableValue(variableComponent.name))
@@ -15,17 +17,14 @@ case class CalculateVisitor(totalValue:Double) extends FunctionVisitor():
     case division: Division =>
       CalculateVisitor(
         CalculateVisitor(0d).visit(division.leftFunctionComponent).totalValue
-          /
-        CalculateVisitor(0d).visit(division.rightFunctionComponent).totalValue)
+          / CalculateVisitor(0d).visit(division.rightFunctionComponent).totalValue)
     case power: Power =>
       CalculateVisitor(
-        CalculateVisitor(0d).visit(power.leftFunctionComponent).totalValue
-          /
-        CalculateVisitor(0d).visit(power.rightFunctionComponent).totalValue)
+        pow(CalculateVisitor(0d).visit(power.leftFunctionComponent).totalValue
+          ,CalculateVisitor(0d).visit(power.rightFunctionComponent).totalValue))
     case subtraction: Subtraction =>
       CalculateVisitor(
         CalculateVisitor(0d).visit(subtraction.leftFunctionComponent).totalValue
-          /
-        CalculateVisitor(0d).visit(subtraction.rightFunctionComponent).totalValue)
+          - CalculateVisitor(0d).visit(subtraction.rightFunctionComponent).totalValue)
     case abs: Abs =>
       CalculateVisitor(CalculateVisitor(0d).visit(abs.functionComponent).totalValue.abs)
