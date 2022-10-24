b5062a3cac04eb70e48715d8b5779aaeddfa08f9
Jorge Lopez Bauer <jorge.lopezbauer@ing.austral.edu.ar>
2022-10-24 11:36:42 -0300

    Sum showFunction

--- src/main/scala/composite/composite/implementation/Sum.scala
+++ src/main/scala/composite/composite/implementation/Sum.scala
@@ -8,3 +8,7 @@ case class Sum(functionComponents: List[FunctionComponent])
   extends CommutativeFunctionOneComponentListFunction(functionComponents: List[FunctionComponent]):
   def calculate: Double =
     functionComponents.foldLeft(0d)((acc, functionComponent) => acc + functionComponent.calculate)
+
+  def showFunction: String =
+    val string = functionComponents.tail.foldLeft(functionComponents.head.showFunction)((acc, functionComponent) => acc + " + " + functionComponent.showFunction)
+    s"($string)"
