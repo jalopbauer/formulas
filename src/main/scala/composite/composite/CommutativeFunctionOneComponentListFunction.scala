6faf8b9df018cd5453b3f110690934601f9ecdc4
Jorge Lopez Bauer <jorge.lopezbauer@ing.austral.edu.ar>
2022-10-24 11:39:08 -0300

    Added missing extends

--- src/main/scala/composite/composite/CommutativeFunctionOneComponentListFunction.scala
+++ src/main/scala/composite/composite/CommutativeFunctionOneComponentListFunction.scala
@@ -3,6 +3,6 @@ package composite.composite
 import composite.FunctionComponent
 import composite.component.VariableComponent
 
-trait CommutativeFunctionOneComponentListFunction(functionComponents: List[FunctionComponent]):
+trait CommutativeFunctionOneComponentListFunction(functionComponents: List[FunctionComponent]) extends FunctionComponent:
   def getVariables: List[VariableComponent] =
     functionComponents.flatMap(_.getVariables)
--- src/main/scala/composite/composite/SingleComponentFunction.scala
+++ src/main/scala/composite/composite/SingleComponentFunction.scala
@@ -3,7 +3,7 @@ package composite.composite
 import composite.FunctionComponent
 import composite.component.VariableComponent
 
-trait SingleComponentFunction(functionComponent: FunctionComponent):
+trait SingleComponentFunction(functionComponent: FunctionComponent) extends FunctionComponent:
   def getVariables:List[VariableComponent] =
     functionComponent.getVariables
 
